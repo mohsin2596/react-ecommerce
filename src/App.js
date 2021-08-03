8c6defc35cba7a2be85ee463d0b8b7db0d7ab509
Mohsin Ajmal <mohsin2596@gmail.com>
2021-08-03 20:12:12 +0500

    Separated homepage components with updated styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homescreen.component';
+import HomePage from './pages/homepage/homescreen.component';
 
 function App() {
   return (
