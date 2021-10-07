e8a022d5a0b173034ea61adc825c341b4ae3fe4d
Mohsin <mohsin2596@gmail.com>
2021-10-07 18:47:18 +0500

    Added header nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homescreen.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component= {HomePage}/>
         <Route exact path='/shop' component= {ShopPage}/>
